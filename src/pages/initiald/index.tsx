aaa10d72f97eb7d8c0c2e0336439ad892413de47
Priambudi Lintang Bagaskara <36256082+priambudiLB@users.noreply.github.com>
2025-02-12 15:17:24 +0800

    add wmmt and ID

---
+++ src/pages/initiald/index.tsx
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React from 'react';
+
+import Layout from '@theme/Layout';
+
+const TITLE = 'Initial D';
+const DESCRIPTION = 'Initial D journey showcase.';
+
+function InitialD() {
+  return (
+    <Layout title={TITLE} description={DESCRIPTION}>
+      <main className="container margin-vert--lg">
+      </main>
+    </Layout>
+  );
+}
+
+export default InitialD;
---
+++ src/pages/wmmt/index.tsx
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React from 'react';
+
+import Layout from '@theme/Layout';
+
+const TITLE = 'WMMT';
+const DESCRIPTION = 'Wangan Midnight Maximum Tune (WMMT) journey showcase.';
+
+function WMMT() {
+  return (
+    <Layout title={TITLE} description={DESCRIPTION}>
+      <main className="container margin-vert--lg">
+      </main>
+    </Layout>
+  );
+}
+
+export default WMMT;
